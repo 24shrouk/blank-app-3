83a709c8a4e330269c7aa05afcd48a41aeddafd4
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-10-17 05:41:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,16 @@ from pages import insights  # Make sure your insights page is correctly imported
 from pages import dashboard_homepage  # Ensure this page is correctly imported
 
 # Set page title and layout
-st.set_page_config(page_title="Telco Customer Churn Dashboard", layout="wide")
+#st.set_page_config(page_title="Telco Customer Churn Dashboard", layout="wide")
 
 # Sidebar for navigation
 st.sidebar.title("Telco Customer Churn")
-page = st.sidebar.selectbox("Select a Page", ["Insights"])
+#page = st.sidebar.selectbox("Select a Page", ["Insights"])
 
 # Navigation logic
 
-if page == "Insights":
-    insights.app()  # Ensure insights has an app() method that renders content
+#if page == "Insights":
+    #insights.app()  # Ensure insights has an app() method that renders content
 #else:
   #  dashboard_homepage.app()  #
 
